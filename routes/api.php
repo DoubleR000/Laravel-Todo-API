52bbc12a1476604dda3a4ed68e75a1b2fe09c35d
Renz Valenzuela <revalenzuelanz.029@gmail.com>
2025-01-05 23:02:36 +0800

    added routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return response()->json([
+        "message" => "Welcome to Todo API!",
+        "made_by" => "Renz Rommel E. Valenzuela",
+        "made_for" => "Practicing purposes."
+    ]);
+});
+
+
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+
+Route::apiResource('todos', TodoController::class);
